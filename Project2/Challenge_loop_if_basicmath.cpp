d4c48fa6a7f9f7f4b57a666bfa5a7684bf59c300
perth pp <kekskcfm@gmail.com>
2020-08-13 22:30:29 +0700

    edit

--- Project2/Challenge_loop_if_basicmath.cpp
+++ Project2/Challenge_loop_if_basicmath.cpp
@@ -3,27 +3,16 @@
 int main()
 {
 	int a, b, n;
-	float x, y, sd, av, sum = 0, sumsd = 0;
+	float x, y, sd{}, av, sum = 0, sumsd = 0;
 	scanf_s("%d %d", &a, &b);
-	if (a > b)
+	n = abs(a - b) + 1;
+	if (a == b)
 	{
-		n = (a - b) + 1;
-		for (int i = 0; i < n; i++)
-		{
-			printf("%d ", a);
-			sum += a;
-			sumsd += a * a;
-			x = n * sumsd - pow(sum, 2);
-			y = n * (n - 1);
-			sd = sqrt(x / y);
-			a--;
-		}
-		printf("\nAverage = %.1f", (sum) / n);
-		printf("\nSD = %.2f", sd);
+		printf("Error");
+		return 0;
 	}
 	else
 	{
-		n = (b - a) + 1;
 		for (int i = 0; i < n; i++)
 		{
 			printf("%d ", a);
@@ -32,10 +21,18 @@ int main()
 			x = n * sumsd - pow(sum, 2);
 			y = n * (n - 1);
 			sd = sqrt(x / y);
-			a++;
+
+			if (a > b)
+			{
+				a--;
+			}
+			else
+			{
+				a++;
+			}
 		}
-		printf("\nAverage = %.1f", (sum) / n);
-		printf("\nSD = %.2f", sd);
 	}
+	printf("\nAverage = %.1f", (sum) / n);
+	printf("\nSD = %.2f", sd);
 	return 0;
 }
