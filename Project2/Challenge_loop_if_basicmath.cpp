9d05627701edcf413128aedc0553bdf0311de215
perth pp <kekskcfm@gmail.com>
2020-08-13 22:10:28 +0700

    end

--- Project2/Challenge_loop_if_basicmath.cpp
+++ Project2/Challenge_loop_if_basicmath.cpp
@@ -1,26 +1,41 @@
 #include<stdio.h>
+#include<cmath>
 int main()
 {
 	int a, b, n;
-	scanf("%d %d", &a, &b);
+	float x, y, sd, av, sum = 0, sumsd = 0;
+	scanf_s("%d %d", &a, &b);
 	if (a > b)
 	{
-		n = a - b;
-		for (int i = 0; i < n + 1; i++)
+		n = (a - b) + 1;
+		for (int i = 0; i < n; i++)
 		{
 			printf("%d ", a);
+			sum += a;
+			sumsd += a * a;
+			x = n * sumsd - pow(sum, 2);
+			y = n * (n - 1);
+			sd = sqrt(x / y);
 			a--;
 		}
+		printf("\nAverage = %.1f", (sum) / n);
+		printf("\nSD = %.2f", sd);
 	}
 	else
 	{
-		n = b - a;
-		for (int i = 0; i < n + 1; i++)
+		n = (b - a) + 1;
+		for (int i = 0; i < n; i++)
 		{
 			printf("%d ", a);
+			sum += a;
+			sumsd += a * a;
+			x = n * sumsd - pow(sum, 2);
+			y = n * (n - 1);
+			sd = sqrt(x / y);
 			a++;
 		}
+		printf("\nAverage = %.1f", (sum) / n);
+		printf("\nSD = %.2f", sd);
 	}
 	return 0;
 }
-	
